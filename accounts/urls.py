3a7efa3d684f2214c4745a71e937f61e8305c2af
djagpal02 <70053549+djagpal02@users.noreply.github.com>
2020-08-28 13:33:55 +0100

    bug fixes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import urls
-from django.contrib.ath import views as auth_views
+from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
-urlpattern = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', name='login'),
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUp.as_view(),name='signup'),
 
